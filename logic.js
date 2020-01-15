209c9b5c1313a9be8ce960497009c82528657b59
Ivan Khuri <ivankhuri@gmail.com>
2020-01-15 17:15:01 +0200

    Filter

--- logic.js
+++ logic.js
@@ -3,97 +3,86 @@
 // For part one we expect you to use tdd
 
 var todoFunctions = {
-  // todoFunctions.generateId() will give you a unique id
-  // You do not need to understand the implementation of this function.
-  generateId: (function() {
-    var idCounter = 0;
+    // todoFunctions.generateId() will give you a unique id
+    // You do not need to understand the implementation of this function.
+    generateId: (function() {
+        var idCounter = 0;
 
-    function incrementCounter() {   
-      return (idCounter += 1);
-    }
+        function incrementCounter() {
+            return (idCounter += 1);
+        }
 
-    return incrementCounter;
-  })(),
-  
-  //cloneArrayOfObjects will create a copy of the todos array 
-  //changes to the new array don't affect the original
-  cloneArrayOfObjects: function(todos) {
-    return todos.map(function(todo){
-      return JSON.parse(JSON.stringify(todo));
-    }); 
-  }, 
-  
-  addTodo: function(todos, newTodo) {
-    // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
-    // returns a new array, it should contain todos with the newTodo added to the end.
-    // add an id to the newTodo. You can use the generateId function to create an id.
-    // hint: array.concat
-    let newTodoObj =[{
-      id: todoFunctions.generateId(),
+        return incrementCounter;
+    })(),
+
+    //cloneArrayOfObjects will create a copy of the todos array 
+    //changes to the new array don't affect the original
+    cloneArrayOfObjects: function(todos) {
+        return todos.map(function(todo) {
+            return JSON.parse(JSON.stringify(todo));
+        });
+    },
+
+    addTodo: function(todos, newTodo) {
+        // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
+        // returns a new array, it should contain todos with the newTodo added to the end.
+        // add an id to the newTodo. You can use the generateId function to create an id.
+        // hint: array.concat
+        let newTodoObj = [{
+            id: todoFunctions.generateId(),
             description: newTodo.description,
             done: false
-    }]
-    return todos.concat(newTodoObj)
-   
-  },
-  deleteTodo: function(todos, idToDelete) {
+        }]
+        return todos.concat(newTodoObj)
 
-    return todos.filter(function (x){
-        return x["id"]!==idToDelete;
-    });
-    // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
-    // return a new array, this should not contain any todo with an id of idToDelete
-    // hint: array.filter
-  },
-  markTodo: function(todos, idToMark) {
-    return todos.map(function(curr) {
-      if(curr["id"]==idToMark) 
-        {
-            if (curr["done"]==false)
-                curr["done"]=true;
-            else
-                curr["done"]=false;
-        }
-        return curr;
-    })
-    // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
-    // in the new todo array, all elements will remain unchanged except the one with id: idToMark
-    // this element will have its done value toggled
-    // hint: array.map
-  },
-  sortTodos: function(todos, sortFunction) {
-    // stretch goal! Do this last
-    // should leave the input arguement todos unchanged (you can use cloneArrayOfObjects)
-    // sortFunction will have same signature as the sort function in array.sort
-    // hint: array.slice, array.sort
-  //sort be Done/unDone 
-  
-return sortFunction(todos)
-  },
-  sortByFalse: function (arr){
-    // sort by false
-   return arr.sort((a, b) => {
-    if (a.done < b.done) return -1
-    return a.done > b.done ? 1 : 0
-  })
-  },
-  
-  sortByTrue: function (arr){
-    // sort by true
-   return arr.sort((a, b) => {
-    if (a.done > b.done) return -1
-    return a.done < b.done ? 1 : 0
-  })
-  },
+    },
+    deleteTodo: function(todos, idToDelete) {
+
+        return todos.filter(function(x) {
+            return x["id"] !== idToDelete;
+        });
+        // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
+        // return a new array, this should not contain any todo with an id of idToDelete
+        // hint: array.filter
+    },
+    markTodo: function(todos, idToMark) {
+        return todos.map(function(curr) {
+                if (curr["id"] == idToMark) {
+                    if (curr["done"] == false)
+                        curr["done"] = true;
+                    else
+                        curr["done"] = false;
+                }
+                return curr;
+            })
+            // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
+            // in the new todo array, all elements will remain unchanged except the one with id: idToMark
+            // this element will have its done value toggled
+            // hint: array.map
+    },
+    sortTodos: function(todos, sortFunction) {
+
+        //sort be Done/unDone 
+
+        return sortFunction(todos)
+    },
+    sortByFalse: function(arr) {
+        // sort by false
+        return arr.sort((a, b) => {
+            if (a.done < b.done) return -1
+            return a.done > b.done ? 1 : 0
+        })
+    },
+
+    sortByTrue: function(arr) {
+        // sort by true
+        return arr.sort((a, b) => {
+            if (a.done > b.done) return -1
+            return a.done < b.done ? 1 : 0
+        })
+    },
 };
 
-
-// Why is this if statement necessary?
-// The answer has something to do with needing to run code both in the browser and in Node.js
-// See this article for more details: 
-// http://www.matteoagosti.com/blog/2013/02/24/writing-javascript-modules-for-both-browser-and-node/
 if (typeof module !== 'undefined') {
-  module.exports = todoFunctions;
+    module.exports = todoFunctions;
 }
-
-
