76b1bc89423092003a42c6923c7ab14ba4d20907
Ivan Khuri <ivankhuri@gmail.com>
2020-01-15 12:18:09 +0200

    Nothing much

--- test.js
+++ test.js
@@ -1,137 +1,125 @@
 var logic = require('./logic');
-
-
 describe('addTodo', function() {
-  it('should do addTodo', function() {
-    var todos = [];
-    var newTodo = { description: 'make smoothie out of things that should really be cooked' };
-    var updatedTodos = logic.addTodo(todos, newTodo);
-    result = [
-         {
-           id: 1,
-           description: "make smoothie out of things that should really be cooked",
-           done: false,
-         }
-       ]
-       console.log("R: ",result)
-       console.log("New R: ",updatedTodos)
-    expect(updatedTodos).toEqual(result);
-  });
+    it('should do addTodo', function() {
+        var todos = [];
+        var newTodo = { description: 'make smoothie out of things that should really be cooked' };
+        var updatedTodos = logic.addTodo(todos, newTodo);
+        result = [{
+            id: 1,
+            description: "make smoothie out of things that should really be cooked",
+            done: false,
+        }]
+        console.log("R: ", result)
+        console.log("New R: ", updatedTodos)
+        expect(updatedTodos).toEqual(result);
+    });
 });
 
 describe('Marking a to do', function() {
-  it('Marking element 1', function() {
-    var expected = [{id: 0, description: 'make tea', done: true},
-                    {id: 1, description: 'make eggs', done: false},
-                    {id: 2, description: 'smash avocado', done: false}]
-    var actual = [{id: 0, description: 'make tea', done: false},
-                  {id: 1, description: 'make eggs', done: false},
-                  {id: 2, description: 'smash avocado', done: false}]
-    expect(expected).toEqual(logic.markTodo(actual,0));
-  });
+    it('Marking element 1', function() {
+        var expected = [{ id: 0, description: 'make tea', done: true },
+            { id: 1, description: 'make eggs', done: false },
+            { id: 2, description: 'smash avocado', done: false }
+        ]
+        var actual = [{ id: 0, description: 'make tea', done: false },
+            { id: 1, description: 'make eggs', done: false },
+            { id: 2, description: 'smash avocado', done: false }
+        ]
+        expect(expected).toEqual(logic.markTodo(actual, 0));
+    });
 });
 describe('delete object', function() {
-  it('should delete the object with the givin id', function() {
-    var expected = [
-      {
-        id: 1,
-        description: 'make coffee',
-        done: false,
-      },
-        {
-        id: 7,
-        description: 'make coffee',
-        done: false,
-      },
-    ];
-    var actual = [
-      {
-        id: 0,
-        description: 'smash avocados',
-        done: true,
-      },
-      {
-        id: 1,
-        description: 'make coffee',
-        done: false,
-      },
-        {
-        id: 7,
-        description: 'make coffee',
-        done: false,
-      },
-    ];
-    expect(expected).toEqual(logic.deleteTodo(actual,0));
-  });
-  });
+    it('should delete the object with the givin id', function() {
+        var expected = [{
+                id: 1,
+                description: 'make coffee',
+                done: false,
+            },
+            {
+                id: 7,
+                description: 'make coffee',
+                done: false,
+            },
+        ];
+        var actual = [{
+                id: 0,
+                description: 'smash avocados',
+                done: true,
+            },
+            {
+                id: 1,
+                description: 'make coffee',
+                done: false,
+            },
+            {
+                id: 7,
+                description: 'make coffee',
+                done: false,
+            },
+        ];
+        expect(expected).toEqual(logic.deleteTodo(actual, 0));
+    });
+});
 
 describe('sortTodosByTrueAnFalse', function() {
-  it('should do sortTodosByTrue', function() {
-    var todos = [
-      {
-        id: 2,
-        description: "make smoothie out of things that should really be cooked",
-        done: false,
-      }
-    ,
-      {
-        id: 1,
-        description: "make smoothie out of things that should really be cooked",
-        done: true,
-      }
-    ];
+    it('should do sortTodosByTrue', function() {
+            var todos = [{
+                    id: 2,
+                    description: "make smoothie out of things that should really be cooked",
+                    done: false,
+                },
+                {
+                    id: 1,
+                    description: "make smoothie out of things that should really be cooked",
+                    done: true,
+                }
+            ];
 
-    var updatedTodos = logic.sortTodos(todos,arr=> logic.sortByTrue(arr))
+            var updatedTodos = logic.sortTodos(todos, arr => logic.sortByTrue(arr))
 
-    result = [
-      {
-        id: 1,
-        description: "make smoothie out of things that should really be cooked",
-        done: true,
-      }
-    ,
-      {
-        id: 2,
-        description: "make smoothie out of things that should really be cooked",
-        done: false,
-      }
-    ];
-       console.log("R: ",result)
-       console.log("New R: ",updatedTodos)
-    expect(updatedTodos).toEqual(result);
-}),
-  it('should do sortTodosByFalse', function() {
-    var todos = [
-      {
-        id: 2,
-        description: "make smoothie out of things that should really be cooked",
-        done: true,
-      }
-    ,
-      {
-        id: 1,
-        description: "make smoothie out of things that should really be cooked",
-        done: false,
-      }
-    ];
+            result = [{
+                    id: 1,
+                    description: "make smoothie out of things that should really be cooked",
+                    done: true,
+                },
+                {
+                    id: 2,
+                    description: "make smoothie out of things that should really be cooked",
+                    done: false,
+                }
+            ];
+            console.log("R: ", result)
+            console.log("New R: ", updatedTodos)
+            expect(updatedTodos).toEqual(result);
+        }),
+        it('should do sortTodosByFalse', function() {
+            var todos = [{
+                    id: 2,
+                    description: "make smoothie out of things that should really be cooked",
+                    done: true,
+                },
+                {
+                    id: 1,
+                    description: "make smoothie out of things that should really be cooked",
+                    done: false,
+                }
+            ];
 
-    var updatedTodos = logic.sortTodos(todos,arr=> logic.sortByFalse(arr))
+            var updatedTodos = logic.sortTodos(todos, arr => logic.sortByFalse(arr))
 
-    result = [
-      {
-        id: 1,
-        description: "make smoothie out of things that should really be cooked",
-        done: false,
-      }
-    ,
-      {
-        id: 2,
-        description: "make smoothie out of things that should really be cooked",
-        done: true,
-      }
-    ];
-       console.log("R: ",result)
-       console.log("New R: ",updatedTodos)
-    expect(updatedTodos).toEqual(result);
-});
+            result = [{
+                    id: 1,
+                    description: "make smoothie out of things that should really be cooked",
+                    done: false,
+                },
+                {
+                    id: 2,
+                    description: "make smoothie out of things that should really be cooked",
+                    done: true,
+                }
+            ];
+            console.log("R: ", result)
+            console.log("New R: ", updatedTodos)
+            expect(updatedTodos).toEqual(result);
+        });
 });
