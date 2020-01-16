ea1a54e7654ab813ee25fb1b83124202ff7d6214
Ivan Khuri <ivankhuri@gmail.com>
2020-01-16 12:52:59 +0200

    Added date

--- js/dom.js
+++ js/dom.js
@@ -1,11 +1,11 @@
-(function() {
+(function () {
     var container = document.getElementById('todo-container');
     var addTodoForm = document.getElementById('add-todo');
 
 
     var state = [];
 
-    var createTodoNode = function(todo) {
+    var createTodoNode = function (todo) {
         var todoNode = document.createElement('li');
         todoNode.setAttribute('style', 'list-style-type:none;');
         var spanNode = document.createElement('span');
@@ -15,7 +15,7 @@
         if (todo.done)
             checkbox.checked = true;
 
-        checkbox.addEventListener('click', function(event) {
+        checkbox.addEventListener('click', function (event) {
             todoFunctions.markTodo(state, todo.id);
         });
 
@@ -27,7 +27,7 @@
         var deleteButtonNode = document.createElement('button');
         deleteButtonNode.textContent = "Delete";
 
-        deleteButtonNode.addEventListener('click', function(event) {
+        deleteButtonNode.addEventListener('click', function (event) {
             var newState = todoFunctions.deleteTodo(state, todo.id);
             update(newState);
         });
@@ -40,7 +40,7 @@
     };
 
     if (addTodoForm) {
-        addTodoForm.addEventListener('submit', function(event) {
+        addTodoForm.addEventListener('submit', function (event) {
             event.preventDefault();
             if (event.target.description.value === '')
                 return;
@@ -52,7 +52,7 @@
         });
     }
 
-    document.getElementById("filter").addEventListener('click', function(event) {
+    document.getElementById("filter").addEventListener('click', function (event) {
         filterMode = document.getElementById("filter").textContent;
         console.log(filterMode)
         if (filterMode.includes("UnDone")) {
@@ -67,20 +67,57 @@
         update(newState);
     });
 
-    var update = function(newState) {
+    var update = function (newState) {
         state = newState;
         renderState(state);
     };
 
-    var renderState = function(state) {
+    var renderState = function (state) {
         var todoListNode = document.createElement('ul');
 
-        state.forEach(function(todo) {
+        state.forEach(function (todo) {
             todoListNode.appendChild(createTodoNode(todo));
         });
 
         container.replaceChild(todoListNode, container.firstChild);
     };
+
+    var dayanddate = function () {
+        var str = ""
+        const date = new Date();
+        switch (date.getDay()) {
+            case 6:
+                str += "Saturday";
+                break;
+            case 0:
+                str += "Sunday";
+                break;
+            case 1:
+                str += "Monday";
+                break;
+            case 2:
+                str += "Tuesday";
+                break;
+            case 3:
+                str += "Wednesday";
+                break;
+            case 4:
+                str += "Thursday";
+                break;
+            case 5:
+                str += "Friday";
+                break;
+        }
+        str = str + " " + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+        var divnode = document.createElement("div");
+        var spannode = document.createElement("span");
+        spannode.textContent = str;
+        divnode.appendChild(spannode);
+        divnode.setAttribute('id', 'date-container');
+        document.getElementById("main-container").insertBefore(divnode, document.getElementById("main-container").firstChild);
+    }
+
+    dayanddate();
 
     if (container) renderState(state);
 })();
